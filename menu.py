50db3bc7188dfd528835720b3eed9a06386e80d4
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-05 16:31:32 +0000

    Added Menu class with items

---
+++ menu.py
@@ -0,0 +1,11 @@
+class Menu:
+    def __init__(self):
+        self.items = [
+            "Bread",
+            "Toasted Bread",
+            "Garlic Bread",
+            "Baguette",
+            "Water",
+            "Sprite",
+            "Coke"
+        ]
